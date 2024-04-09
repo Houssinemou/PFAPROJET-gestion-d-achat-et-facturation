98840d20281843097f55ed1c12d222bc989b679a
houssine mouram <houssinemouram2003@gmail.com>
2024-04-09 02:06:08 +0000

    first commit

---
+++ gestion-de-vente-facturation/src/main/java/com/emsi/gestion/de/vente/facturation/Application.java
@@ -0,0 +1,13 @@
+package com.emsi.gestion.de.vente.facturation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ gestion-de-vente-facturation/src/test/java/com/emsi/gestion/de/vente/facturation/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emsi.gestion.de.vente.facturation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
